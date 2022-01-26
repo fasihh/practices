f8fd3c4e6a0890b0dd5918db6f43265d70343fcf
Fasih Khan <47947561+fasihh@users.noreply.github.com>
2022-01-26 21:13:40 +0500

    Add files via upload

--- Module1.vb
+++ Module1.vb
@@ -1,49 +1,47 @@
 ﻿Module Module1
-    Dim topOfStack As Integer = 9
-    Dim bottomOfStack As Integer = 0
-    Dim Stack(9) As String
-    Dim StackPtr As Integer = 0
-
+    Dim FrontPointer As Integer
+    Dim BackPointer As Integer
+    Dim Queue(9) As String
 
     Sub Main()
         Dim choice As Integer
 
-        Do While choice > 4 Or choice < 1
-            Console.WriteLine("Please enter your option: ")
-            Console.WriteLine("1. Pop")
-            Console.WriteLine("2. Push")
-
-            choice = Console.ReadLine()
+        Do While choice <= 4 Or choice >= 1
+            Console.Write("Input choice: ")
+            choice = Console.ReadLine
 
             If choice = 1 Then
-                Call Pop()
-                Call Main()
-            End If
-            If choice = 2 Then
-                Call Push()
-                Call Main()
+                Add()
+                Main()
+            ElseIf choice = 2 Then
+                Delete()
+                Main()
+            ElseIf choice = 3 Then
+                For index = BackPointer To (FrontPointer - 1)
+                    Console.WriteLine(Queue(index))
+                Next
             End If
         Loop
 
     End Sub
-
-    Sub Pop()
-        If StackPtr = bottomOfStack Then
-            Console.WriteLine("Underflow")
-        Else
-            StackPtr = StackPtr - 1
-            Console.WriteLine(Stack(1))
-        End If
-
-    End Sub
-    Sub Push()
-        Dim newVal As String
-        Console.Write("Enter New Value: ")
-        newVal = Console.ReadLine
-        If StackPtr > topOfStack Then
+    Sub Add()
+        If FrontPointer = 10 Then
             Console.WriteLine("Overflow")
+            Exit Sub
         End If
-        Stack(StackPtr) = newVal
-        StackPtr = StackPtr + 1
+        Dim data As String
+        Console.Write("Input data to be added: ")
+        data = Console.ReadLine()
+        Queue(FrontPointer) = data
+        FrontPointer = FrontPointer + 1
+    End Sub
+    Sub Delete()
+        If BackPointer = FrontPointer Then
+            Console.WriteLine("Underflow")
+            Exit Sub
+        End If
+        Dim data As String
+        data = Queue(BackPointer)
+        BackPointer = BackPointer + 1
     End Sub
 End Module
